3cc22ff3632239db834ca7be028bea10d6add3cf
Philip Andersson <philip.andersson.1991@hotmail.com>
2023-11-29 21:03:09 +0100

    någotVajsing

--- functions/getNotes/index.js
+++ functions/getNotes/index.js
@@ -2,8 +2,12 @@ const AWS = require('aws-sdk');
 const db = new AWS.DynamoDB.DocumentClient();
 const {sendResponse} = require('../../responses');
 const { default: middy } = require('@middy/core');
+const { validateToken } = require('../middleware/auth');
 
 const getNotes = async (event, context) => {
+
+    if (event?.error && event?.error === '401')
+        return sendResponse(401, {success: false, message: 'Invalid token'});
 
     
     let params = {
@@ -11,7 +15,7 @@ const getNotes = async (event, context) => {
         FilterExpression: 'uid = :uidValue AND active = :activeValue',
         ExpressionAttributeValues: {
             ':uidValue': "philipTest",
-            'activeValue': true
+            ':activeValue': true
         }
     }
 
@@ -26,6 +30,7 @@ const getNotes = async (event, context) => {
     }
 }
 
-const handler = middy(getNotes);
+const handler = middy(getNotes)
+    .use(validateToken)
 
-module.exports = {handler}
+module.exports = { handler };
